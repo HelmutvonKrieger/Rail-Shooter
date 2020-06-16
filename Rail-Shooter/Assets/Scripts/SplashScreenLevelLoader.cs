ebfb16f8517baf4c8dd82d590ccaef67338ce766
HelmutvonKrieger <crissbrettd@outlook.com>
2020-06-15 20:50:02 -0700

    Adding music and initial camera circuit

---
+++ Rail-Shooter/Assets/Scripts/SplashScreenLevelLoader.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashScreenLevelLoader : MonoBehaviour
+{
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        handleSplashScreenInput();
+    }
+
+    private void handleSplashScreenInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Invoke("loadSceneAsync", 1f);
+        }
+    }
+
+    private void loadSceneAsync()
+    {
+        SceneManager.LoadSceneAsync("Main Menu");
+    }
+}
